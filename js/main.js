5a173f59dfd348b86d78abce00b3c38cc1f440af
Ainhoa PG <ainhoapalacio>
2021-05-05 20:20:34 +0200

    listado + ul +lis

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 // Añadir el listado original
+document.getElementById("new").addEventListener("click", function () {
+  let task = document.getElementById("text").value;
+  let priority = document.getElementById("priority").value;
+  // console.log(priority);
+  if (task && priority) {
+    addNewTask(task, priority);
+    document.getElementById("text").value = "";
+  }
+});
+
+// console.log(todos);
+todos.forEach(function (todo) {
+  addNewTask(todo.text, todo.priority);
+}),
+  function removeItem() {
+    let item = this.parentNode.parentNode; // los lis
+    let parent = item.parentNode; // el ul
+
+    parent.removeChild(item);
+  };
 
 // Añadir una tarea nueva
 
